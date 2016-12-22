89932e9742ab98f103295bf6d5994a84083c3035
Melissa Reynolds <mkreynolds@cableone.net>
2016-12-22 03:30:16 +0000

    Added sections - lesson 17

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161221212018) do
+ActiveRecord::Schema.define(version: 20161222023230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20161221212018) do
   end
 
   add_index "courses", ["user_id"], name: "index_courses_on_user_id", using: :btree
+
+  create_table "sections", force: true do |t|
+    t.string   "title"
+    t.integer  "course_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
